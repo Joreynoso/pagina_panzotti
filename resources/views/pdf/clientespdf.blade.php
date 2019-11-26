e3e1498788f1482bbb42700da616ce64fed4364f
jose.developer <jose.developer@gmail.com>
2019-11-26 09:21:53 -0300

    importacion plantilla, geolocalizacion de google maps.

--- resources/views/pdf/clientespdf.blade.php
+++ resources/views/pdf/clientespdf.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,14 +8,30 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
 </head>
+
+<style>
+    body {
+        font-size: 12px;
+    }
+</style>
+
 <body>
 
-    <div class="jumbotron bg-white jumbotron-fluid">
-        <div class="container">
-            <h1 class="display-4">Pastas Panzotti.</h1>
-            <p class="lead">Tabla clientes de la empresa. 2019.</p>
+    <div class="row">
+        <div class="col-12 text-center mt-3">
+            <img src="{{ asset('img/1574726509.jpg') }}" alt="" style="width: 200px;">
         </div>
+    </div>
+
+    <div class="container">
+        <h1 class="display-4">Pastas Panzotti.</h1>
+        <p>Direccion de la empresa: Prado 898.</p>
+        <p>San Fernando del Valle de Catarmarca, Provincia de Catamarca. Argentina (2019)</p>
+        <p>El presente informe detalla toda la informacion referente a los
+            Clientes de la empresa "pastas Panzotti", el mismo corresponde al periodo de
+            tiempo 25/11/2019 hasta el dia de la fecha 26/11/2019.</p>
     </div>
 
     <div class="container">
@@ -46,5 +63,24 @@
         </div>
     </div>
 
+    <div class="row text-center">
+        <div class="container">
+            <div class="col-4 text-center mt-4">
+                <p>firma presidente de la empresa.</p>
+                <p class="mt-4">.............................................</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="container">
+            <div class="col-12 mt-4 text-center">
+                <p>el presente documento sirve como comprobante de las actividades que
+                    realiza la empresa, ante las autoridades que asi lo requieran.</p>
+            </div>
+        </div>
+    </div>
+
 </body>
+
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,243 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Pagina Web Panzotti Pastas!</title>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="{!! asset('fonts/ionicons.min.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/bootstrap.min.css') !!}">
 
-            .full-height {
-                height: 100vh;
-            }
+    <!-- estilos -->
+    <script src="https://kit.fontawesome.com/a6f4c22e9b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{!! asset('css/scrolling-nav.css') !!}">
+    <link rel="stylesheet" href="{!! asset('css/estilos2.css') !!}">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    <script src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="{!! asset('fonts/beyond_the_mountains-webfont.css') !!}">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600" rel="stylesheet">
+</head>
 
-            .position-ref {
-                position: relative;
-            }
+<body id="page-top">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger font-beyond" href="#page-top">Panzotti</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#services">LOGIN</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#services">PRODUCTOS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#services">RECETAS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="#contact">LOGIN</a>
+                    </li>
+                    <li class="nav-item">
+                        <button class="btn btn-danger ml-2">pedidos: 3834-7799661</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
-            .content {
-                text-align: center;
-            }
+    <header class="bg-primary text-white">
+        <div class="container text-center">
+            <h1 class="mt-5 texto">Panzotti</h1>
+            <p class="lead">Proba nuestras riquisimas pastas caseras, elaboradas con productos naturales!</p>
+            <button class="btn btn-danger">COMENZAR</button>
+        </div>
+    </header>
 
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <div class="heading">
+                        <img class="heading-img" src="img/heading_logo.png" alt="">
+                        <h2>Our Story</h2>
+                    </div>
+                    <p class="lead text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio
+                        fugiat
+                        voluptatem dolor,
+                        provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa
+                        exercitationem, in, quo totam.</p>
                 </div>
             </div>
         </div>
-    </body>
+    </section>
+
+    <!-- tarjetas productos -->
+    <section id="services" class="bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <div class="heading">
+                        <img class="heading-img" src="img/heading_logo.png" alt="">
+                        <h2>Nuestros Productos</h2>
+                        <p class="lead">te mostramos algunos de nuestros productos! </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-xl-3 col-lg-4 col-md-6 mb-4 mt-3">
+                    <div class="blockquote-custom-icon bg-success shadow-sm"><i class="fas fa-seedling text-white"></i>
+                    </div>
+                    <div class="bg-white shadow rounded overflow p-3 tarjeta">
+                        <img src="img/1574600669.jpeg" alt="ft_producto" class="img-fluid card-img-top">
+                        <p class="text-muted mb-2 mt-2">Fetuccine</p>
+                        <p>descripcion del producto</p>
+                        <a href="" title="editar" class="btn btn-sm btn-rojo text-white mb-3">
+                            <i class="fa fa-shopping-cart"></i>
+                            ver detalle
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-4 col-md-6 mb-4 mt-3">
+                    <div class="blockquote-custom-icon bg-success shadow-sm"><i class="fas fa-seedling text-white"></i>
+                    </div>
+                    <div class="bg-white shadow rounded overflow p-3 tarjeta">
+                        <img src="img/1574600669.jpeg" alt="ft_producto" class="img-fluid card-img-top">
+                        <p class="text-muted mb-2 mt-2">Fetuccine</p>
+                        <p>descripcion del producto</p>
+                        <a href="" title="editar" class="btn btn-sm btn-rojo text-white mb-3">
+                            <i class="fa fa-shopping-cart"></i>
+                            ver detalle
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-4 col-md-6 mb-4 mt-3">
+                    <div class="blockquote-custom-icon bg-success shadow-sm"><i class="fas fa-seedling text-white"></i>
+                    </div>
+                    <div class="bg-white shadow rounded overflow p-3 tarjeta">
+                        <img src="img/1574600669.jpeg" alt="ft_producto" class="img-fluid card-img-top">
+                        <p class="text-muted mb-2 mt-2">Fetuccine</p>
+                        <p>descripcion del producto</p>
+                        <a href="" title="editar" class="btn btn-sm btn-rojo text-white mb-3">
+                            <i class="fa fa-shopping-cart"></i>
+                            ver detalle
+                        </a>
+                    </div>
+                </div>
+
+                <div class="col-xl-3 col-lg-4 col-md-6 mb-4 mt-3">
+                    <div class="blockquote-custom-icon bg-success shadow-sm"><i class="fas fa-seedling text-white"></i>
+                    </div>
+                    <div class="bg-white shadow rounded overflow p-3 tarjeta">
+                        <img src="img/1574600669.jpeg" alt="ft_producto" class="img-fluid card-img-top">
+                        <p class="text-muted mb-2 mt-2">Fetuccine</p>
+                        <p>descripcion del producto</p>
+                        <a href="" title="editar" class="btn btn-sm btn-rojo text-white mb-3">
+                            <i class="fa fa-shopping-cart"></i>
+                            ver detalle
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    <div class="heading">
+                        <img class="heading-img" src="img/heading_logo.png" alt="">
+                        <h2>Our Story</h2>
+                    </div>
+                    <p class="lead text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero odio
+                        fugiat
+                        voluptatem dolor,
+                        provident officiis, id iusto! Obcaecati incidunt, qui nihil beatae magnam et repudiandae ipsa
+                        exercitationem, in, quo totam.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Localizacion -->
+    <section id="localizacion" class="bg-light">
+        <div class="map-clean text-center">
+            <div class="container">
+                <div class="intro">
+                    <h2 class="text-center">Donde Encontarnos?</h2>
+                    <p class="text-center">Nunc luctus in metus eget fringilla. Aliquam sed justo ligula. Vestibulum
+                        nibh erat,
+                        pellentesque ut laoreet vitae. </p>
+                </div>
+            </div><iframe allowfullscreen="" frameborder="0" width="80%" height="450"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1753.7218410382939!2d-65.78581202910586!3d-28.46618199860062!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x942428958592b1cd%3A0x514587c1badea74!2sN%C3%BA%C3%B1ez%20del%20Prado%20135%2C%20K4703BKJ%20San%20Fernando%20del%20Valle%20de%20Catamarca%2C%20Catamarca!5e0!3m2!1ses!2sar!4v1574770128462!5m2!1ses!2sar"
+                width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <div class="footer-dark text-white py-5">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">Web design</a></li>
+                            <li><a href="#">Development</a></li>
+                            <li><a href="#">Hosting</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>Company Name</h3>
+                        <p>Praesent sed lobortis mi. Suspendisse vel placerat ligula. Vivamus ac sem lacus. Ut vehicula
+                            rhoncus
+                            elementum. Etiam quis tristique lectus. Aliquam in arcu eget velit pulvinar dictum vel in
+                            justo.</p>
+                    </div>
+                    <div class="col item social">
+                        <a href="#"><i class="icon ion-social-facebook"></i></a>
+                        <a href="#"><i class="icon ion-social-instagram"></i></a>
+                    </div>
+                </div>
+                <p class="copyright">Company Name © 2017</p>
+            </div>
+        </footer>
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="{!! asset('js/bootstrap.bundle.min.js') !!}" async></script>
+    <script type="text/javascript" src="{!! asset('js/jquery.min.js') !!}" async></script>
+
+    <!-- Plugin JavaScript -->
+    <script type="text/javascript" src="{!! asset('js/jquery.easing.min.js') !!}" async></script>
+
+    <!-- js -->
+    <script type="text/javascript" src="{!! asset('js/scrolling-nav.js') !!}" async></script>
+
+</body>
+
 </html>
