a25e7c72b1b9d46063126fb5384963d1ab2060e3
jorgea306 <jorgea306@gmail.com>
2019-12-08 18:01:03 -0300

    Rol Empleado Middleware

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_admin')->default(false);
+            $table->boolean('is_empleado')->default(false);
             $table->rememberToken();
             $table->timestamps();
         });
